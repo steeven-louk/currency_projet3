14bb69d87607c30ba0979bffadbbc273eb626017
steeven-louk <steevenlouk.sl@gmail.com>
2023-07-18 21:20:38 +0200

    initialisation du projet, integration de vue.js et execution des migrations

---
+++ database/seeders/DeviseTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Devises;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DeviseTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $devises =[
+            ['name'=> 'USD'],
+            ['name'=> 'EUR'],
+            ['name'=> 'BTC']
+        ];
+
+        DB::table('Devises')->insert($devises);
+    }
+}
